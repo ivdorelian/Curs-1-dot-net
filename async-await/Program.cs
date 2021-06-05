cd69364a9f01c43e27639259b64d353088d09dba
Ionescu Vlad <ionescu.vlad1@gmail.com>
2021-06-05 11:16:54 +0300

    Curs 5: Async / await console

---
+++ async-await/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace async_await
+{
+    class Program
+    {
+        static int GetFibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+        }
+
+        static Task<int> GetFibnacciAsync(int n)
+        {
+            return Task.Run(() => GetFibonacci(n));
+        }
+
+
+        static async Task Main(string[] args)
+        {
+            // to read: https://www.i-programmer.info/programming/c/1514-async-await-and-the-ui-problem.html
+
+            while (true)
+            {
+                Console.WriteLine("Dati n pentru fibo(n): ");
+                int n = int.Parse(Console.ReadLine());
+
+                Task.Run(async () =>
+                {
+                    var fiboResult = await GetFibnacciAsync(n);
+                    Console.WriteLine($"fibo({n})=" + fiboResult);
+                });
+
+                Console.WriteLine("Cum te cheama?");
+                string raspuns = Console.ReadLine();
+                Console.WriteLine($"Salut, {raspuns}");
+            }
+        }
+    }
+}
